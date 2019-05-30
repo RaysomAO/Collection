970d55ba2cfb8b452f3dff6cf5cc00bae6a1aaba
20150309 <20150309@DESKTOP-32J7ERJ>
2019-05-30 16:55:02 -0400

    Trainable class is up

---
+++ CollectionGames/Dragalia/logica/Trainable.java
@@ -0,0 +1,91 @@
+package logica;
+
+public abstract class Trainable {
+	
+	String Name;
+	int Star;
+	int Level;
+	int BaseHP;
+	int MaxHp;
+	int BaseStrg;
+	int MaxStrg;
+	int GrowHp;//After 55 Round up update getter
+	int GrowStrg;
+	int Exp;
+	int TotalExp;
+	int Maxexp;//Update all ecuations
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public int getStar() {
+		return Star;
+	}
+	public void setStar(int star) {
+		Star = star;
+	}
+	public int getLevel() {
+		return Level;
+	}
+	public void setLevel(int level) {
+		Level = level;
+	}
+	public int getBaseHP() {
+		return BaseHP;
+	}
+	public void setBaseHP(int baseHP) {
+		BaseHP = baseHP;
+	}
+	public int getMaxHp() {
+		return MaxHp;
+	}
+	public void setMaxHp(int maxHp) {
+		MaxHp = maxHp;
+	}
+	public int getBaseStrg() {
+		return BaseStrg;
+	}
+	public void setBaseStrg(int baseStrg) {
+		BaseStrg = baseStrg;
+	}
+	public int getMaxStrg() {
+		return MaxStrg;
+	}
+	public void setMaxStrg(int maxStrg) {
+		MaxStrg = maxStrg;
+	}
+	public int getGrowHp() {
+		return GrowHp;
+	}
+	public void setGrowHp(int growHp) {
+		GrowHp = growHp;
+	}
+	public int getGrowStrg() {
+		return GrowStrg;
+	}
+	public void setGrowStrg(int growStrg) {
+		GrowStrg = growStrg;
+	}
+	public int getExp() {
+		return Exp;
+	}
+	public void setExp(int exp) {
+		Exp = exp;
+	}
+	public int getTotalExp() {
+		return TotalExp;
+	}
+	public void setTotalExp(int totalExp) {
+		TotalExp = totalExp;
+	}
+	public int getMaxexp() {
+		return Maxexp;
+	}
+	public void setMaxexp(int maxexp) {
+		Maxexp = maxexp;
+	}
+
+	
+}
